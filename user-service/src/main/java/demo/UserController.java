08337195b45e6b03f1995c9b157e9de5384e37d4
LoserGhost <s7070106@kmitl.ac.th>
2017-03-06 21:58:07 +0700

    add public .js/.html file , fix code for client

--- user-service/src/main/java/demo/UserController.java
+++ user-service/src/main/java/demo/UserController.java
@@ -3,11 +3,10 @@ package demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     private final UserRepository userRepository;
