f36f4c742b2564ac16584e66952caeb905f28f4d
Nattakit W <ohmguybands@gmail.com>
2017-03-13 23:06:07 +0700

    view, update, deleter user

---
+++ FootballReservation/admin-service/src/main/java/demo/AdminServiceApplication.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * By Nattakit 57070033
+ */
+
+@SpringBootApplication
+public class    AdminServiceApplication {
+
+    public static void main(String ... args) {
+        SpringApplication.run(AdminServiceApplication.class, args);
+    }
+
+}
