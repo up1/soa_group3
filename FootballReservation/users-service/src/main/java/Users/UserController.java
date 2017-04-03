fe0dbcd78f7443f6c86eb294627a0d1d808962b2
Watthakorn <watthakorn01@hotmail.com>
2017-04-03 23:01:54 +0700

    login to get json

--- FootballReservation/users-service/src/main/java/Users/UserController.java
+++ FootballReservation/users-service/src/main/java/Users/UserController.java
@@ -15,34 +15,34 @@ public class UserController {
     @Autowired
     public UserController(UserRepository UserRepository){ this.UserRepository = UserRepository; }
 
-    @RequestMapping("/user/{id}")
+    @RequestMapping(value = "/user/{id}",method = RequestMethod.GET)
     public User getUser(@PathVariable int id) {
         return this.UserRepository.findById((long) id);
     }
-    @RequestMapping("/users")
+    @RequestMapping(value = "/users",method = RequestMethod.GET)
     public List<User> getUsers(
             @RequestParam(value="page", defaultValue="1") int page,
             @RequestParam(value="item_per_page", defaultValue="10") int itemPerPage) {
         return this.UserRepository.findAllUser(page, itemPerPage);
     }
-    @RequestMapping("/user")
+    @RequestMapping(value = "/user" , method = {RequestMethod.GET, RequestMethod.POST})
     public User loginUser(
             @RequestParam(value="username") String username,
             @RequestParam(value="password") String password) {
         return this.UserRepository.login(username,password);
     }
 
-    @RequestMapping("/user/add")
-    public void addUser(User user) {
+    @RequestMapping(value = "/user/add", method = RequestMethod.POST)
+    public void addUser(@RequestBody User user) {
         this.UserRepository.save(user);
     }
 
-    @RequestMapping("/user/{id}/delete")
+    @RequestMapping(value = "/user/{id}/delete", method = RequestMethod.DELETE)
     public void deleteUser(@PathVariable Long id) {
         this.UserRepository.delete(id);
     }
 
-    @RequestMapping("/user/{id}/update")
+    @RequestMapping(value = "/user/{id}/update", method = RequestMethod.PUT)
     public void updateUser(User user,@PathVariable Long id) {
         this.UserRepository.update(user,id);
     }
