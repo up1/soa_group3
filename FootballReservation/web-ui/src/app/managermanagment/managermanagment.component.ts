2ad6c30aac96a4d957314a18b8001ebb9d03a7a6
Pipat Saweay <moseding@hotmail.com>
2017-05-05 23:30:38 +0700

    manager management

--- FootballReservation/web-ui/src/app/managermanagment/managermanagment.component.ts
+++ FootballReservation/web-ui/src/app/managermanagment/managermanagment.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {AdminService} from 'app/_services/index';
 import {User} from 'app/data/user'; 
 
+import { SubStadium } from 'app/data/substadium';
+import { StadiumService } from 'app/data/stadium.service';
+
+
 @Component({
   selector: 'app-managermanagment',
   templateUrl: './managermanagment.component.html',
@@ -10,7 +14,11 @@ import {User} from 'app/data/user';
 export class ManagermanagmentComponent implements OnInit {
 
   users: User[];
-  constructor(private adminservice: AdminService) {}
+  modalWork: boolean;
+  soda: SubStadium[];
+  constructor(private adminservice: AdminService, private _stadiumService: StadiumService) {
+    this.modalWork = false;
+  }
 
   getByRole(role: number): void {
     this.adminservice.getByRole(role).subscribe(
@@ -20,9 +28,32 @@ export class ManagermanagmentComponent implements OnInit {
     )
   }
 
+    deleteById(id: number): void{
+    this.adminservice.delete(id).subscribe();
+    location.reload();
+  }
+
+
 
   ngOnInit() {
     this.getByRole(2);
   }
+
+    toggleBooking(field_id: String) {
+    if (this.modalWork === false) {
+      this.modalWork = true;
+      // this.getSubStadium(field_id)
+    }
+    else if (this.modalWork === true) {
+      this.modalWork = false;
+    }
+  }
+
+  //   getSubStadium(field_id: String) {
+  //   this._stadiumService.getSubStadium(field_id)
+  //     .subscribe(
+  //     soda => this.soda = soda,
+  //     );
+  // }
 
 }
