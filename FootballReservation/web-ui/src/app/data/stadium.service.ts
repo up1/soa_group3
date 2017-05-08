a35977502990a36dc89e24dd9aefe4317953c428
watthakorn <watthakorn01@hotmail.com>
2017-05-08 20:42:27 +0700

    Change to deploy version.

--- FootballReservation/web-ui/src/app/data/stadium.service.ts
+++ FootballReservation/web-ui/src/app/data/stadium.service.ts
@@ -14,63 +14,63 @@ export class StadiumService {
 
   getStadiums(): Observable<Stadium[]> {
     return this._http
-      .get('http://localhost:9001/listfield')
+      .get('http://139.59.123.175:9001/listfield')
       .map((response: Response) => <Stadium[]>response.json());
   }
 
   getStadium(field_id: number): Observable<Stadium[]> {
     return this._http
-      .get('http://localhost:9001/field?field_id=' + field_id.toString())
+      .get('http://139.59.123.175:9001/field?field_id=' + field_id.toString())
       .map((response: Response) => <Stadium[]>response.json());
   }
 
   getSubStadium(field_id: number): Observable<SubStadium[]> {
     return this._http
-      .get('http://localhost:9001/field/' + field_id.toString())
+      .get('http://139.59.123.175:9001/field/' + field_id.toString())
       .map((response: Response) => <SubStadium[]>response.json());
   }
 
   getSubStadiumList(field_id: number): Observable<SubStadium[]> {
     return this._http
-      .get('http://localhost:9003/field/' + field_id.toString())
+      .get('http://139.59.123.175:9003/field/' + field_id.toString())
       .map((response: Response) => <SubStadium[]>response.json());
   }
 
   getSubStadiumData(ex_id: number): Observable<SubStadium[]> {
     return this._http
-      .get('http://localhost:9003/field_ex/' + ex_id.toString())
+      .get('http://139.59.123.175:9003/field_ex/' + ex_id.toString())
       .map((response: Response) => <SubStadium[]>response.json());
   }
 
 
   createSubStadium(field_id: number,fieldex_name: string, rent: number, size: string, floor: string) {
     return this._http
-      .post('http://localhost:9003/field_ex/add', JSON.parse('{"field_id": '+field_id+',"fieldex_name": "' + fieldex_name + '","rent": ' + rent.toString() + ',"image": "img","size": "' + size + '","floor": "' + floor + '"}'))
+      .post('http://139.59.123.175:9003/field_ex/add', JSON.parse('{"field_id": '+field_id+',"fieldex_name": "' + fieldex_name + '","rent": ' + rent.toString() + ',"image": "img","size": "' + size + '","floor": "' + floor + '"}'))
       .map((response: Response) => response.json());
   }
 
   editSubStadium(ex_id: number, fieldex_name: string, rent: number, size: string, floor: string) {
     return this._http
-      .put('http://localhost:9003/field_ex/' + ex_id.toString() + '/update', JSON.parse('{"fieldex_name": "' + fieldex_name + '","rent": ' + rent.toString() + ',"image": "img","size": "' + size + '","floor": "' + floor + '"}'))
+      .put('http://139.59.123.175:9003/field_ex/' + ex_id.toString() + '/update', JSON.parse('{"fieldex_name": "' + fieldex_name + '","rent": ' + rent.toString() + ',"image": "img","size": "' + size + '","floor": "' + floor + '"}'))
       .map((response: Response) => response.json());
   }
 
   deleteSubStadium(ex_id: number) {
     return this._http
-      .delete('http://localhost:9003/field_ex/' + ex_id.toString() + '/delete')
+      .delete('http://139.59.123.175:9003/field_ex/' + ex_id.toString() + '/delete')
       .map((response: Response) => response.json());
   }
 
   getReservebyUserId(user_id: number) {
     return this._http
-      .get('http://localhost:9004/reservation?user_id=' + user_id.toString())
+      .get('http://139.59.123.172:9004/reservation?user_id=' + user_id.toString())
       .map((response: Response) => <Reservation[]>response.json());
   }
 
   PostReserveStadium(user_id: number, field_id: number, ex_id: number, start_time: number, end_time: number, date: String) {
     //wait for edit data
      return this._http
-      .post('http://localhost:9004/reserv', JSON.parse('{"reserv_user_id": '+user_id+',"reserv_field_id": "' + field_id + '","reserv_ex_id": ' + ex_id.toString() + ',"reserv_start_time": '+ start_time.toString() +',"reserv_end_time": ' + end_time.toString() + ',"reserv_date": "' + date + '"}'))
+      .post('http://139.59.123.172:9004/reserv', JSON.parse('{"reserv_user_id": '+user_id+',"reserv_field_id": "' + field_id + '","reserv_ex_id": ' + ex_id.toString() + ',"reserv_start_time": '+ start_time.toString() +',"reserv_end_time": ' + end_time.toString() + ',"reserv_date": "' + date + '"}'))
       .map((response: Response) => response.json());
   }
 
@@ -85,7 +85,7 @@ export class StadiumService {
   //           .map((response: Response) => response.json());
   // }
   getSubStadiumByUser(username: String): Observable<Stadium>{
-     return this._http.post('http://localhost:9001/fieldmanage', JSON.parse('{"username":"'+ username +'"}'))
+     return this._http.post('http://139.59.123.175:9001/fieldmanage', JSON.parse('{"username":"'+ username +'"}'))
             .map((response: Response) => response.json());
   }
 }
